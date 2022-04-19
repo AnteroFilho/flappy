aff83a43ad6ef3f2971475a4c83342bb67a3ffe5
Antero Filho <anteroa.martinsf@gmail.com>
2022-04-19 13:11:27 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -83,6 +83,8 @@ function Passaro(alturaJogo) {
 
     window.onkeydown = e => voando = true 
     window.onkeyup = e => voando = false
+    window.touchstart = e => voando = true
+    window.ontouchend  = e => voando = false
     this.animar = () => {
         const novoY = this.getY() + (voando ? 8 : -5)
         const alturaMaxima = alturaJogo - this.elemento.clientHeight
