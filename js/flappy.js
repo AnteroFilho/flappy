5f2d7ea56fe123f737fd9ff4194cdd605cdea5fb
Antero Filho <anteroa.martinsf@gmail.com>
2022-04-19 13:34:09 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -141,7 +141,7 @@ function FlappyBird(){
     const largura = areaDoJogo.clientWidth
 
     const progresso = new Progresso()
-    const barreiras = new Barreiras(altura, largura, 200, 400, 
+    const barreiras = new Barreiras(altura, largura, 400, 400, 
         () => progresso.atualizarPontos(++pontos))
     const passaro = new Passaro(altura)
 
